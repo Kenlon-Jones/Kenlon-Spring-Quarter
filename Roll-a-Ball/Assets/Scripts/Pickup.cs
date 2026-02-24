d00c77886fd72a8ed7e516bcab6e99462e5cca3b
Kenlon-Jones <kenlon.jones@hgs.hiddengeniusproject.org>
2026-02-23 18:01:27 -0800

    Roll-a-Ball Updated
    Updated

--- Roll-a-Ball/Assets/Scripts/Pickup.cs
+++ Roll-a-Ball/Assets/Scripts/Pickup.cs
@@ -5,16 +5,29 @@ public class Pickup : MonoBehaviour
     // Behavior 1: Destroy this object when touched by the player
     // Behavior 2: Continously Rotate
 
-    [SerializedField, Tooltip("Controls the rotation speed of this pickup.")]
+    [SerializeField, Tooltip("Controls the rotation speed of this pickup.")]
     private float rotateSpeed = 10f;
 
-    // This function is called when a collider hits this pickup
+    // This function is called when a collider hits this pickup only when its own collider is marked as a trigger
     private void OnTriggerEnter(Collider other)
     {
         // Check if we hit the player
         PlayerController controller = other.GetComponent<PlayerController>();
 
         // Try to store the player's player controller component 
+        if (controller != null) // We have hit the player
+        {
+            //Destroy this pickup
+            Destroy(gameObject);
         
+
+
+        }
+    }
+
+    private void Update()
+    {
+        // make the pickup rotate 15 degreeson the x, 45 on the y, and 60 on the z
+        transform.Rotate(new Vector3(15, 45, 60) * rotateSpeed * Time.deltaTime, Space.Self);
     }
 }
--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -5,29 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 5f; // Variable to controll how fast the player moves
-    public CharacterController controller; // Empty reference to thr CharacterController component on the player
+    public Rigidbody rb; // Empty reference to thr CharacterController component on the player
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        controller = GetComponent<CharacterController>(); // Get the CharacterController component attactched to the player 
+        rb = GetComponent<Rigidbody>(); // Get the CharacterController component attactched to the player 
     }
-
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal"); // Stores the Horizontal(Left/Right) input of the player
         float verticalInput = Input.GetAxis("Vertical"); // Stores the Vertical (Up/Down) input of the player
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput); // Calculate the direction the player sgould move based on the input
-        movement = transform.TransformDirection(movement); // Convert the direction; this means 'forward' will be = to direction the player is facing
-        movement *= movementSpeed;
-
-        controller.Move(movement * Time.deltaTime); // Move the player with the characterController component
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb.AddForce(movement*movementSpeed*Time.deltaTime, ForceMode.Impulse);
     }
 }
